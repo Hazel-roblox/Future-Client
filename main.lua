329221b6eb1af41edba1957ce540020e064fe33f
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-03-17 22:14:04 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded() and shared.Future.GuiLibrary ~= nil
 local Future = shared.Future
 local GuiLibrary = Future.GuiLibrary
 local lplr = game.Players.LocalPlayer
@@ -7,6 +7,7 @@ local cam = workspace.CurrentCamera
 local getcustomasset = GuiLibrary.getRobloxAsset
 local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+local fu
 local function wrap(func)
 	local function toWrap()
 		func()
