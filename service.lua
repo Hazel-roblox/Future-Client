8185d9f022c7324479435e0052b8c6530564fdf5
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-04-29 20:05:01 -0400

    Create service.lua

---
+++ service.lua
@@ -0,0 +1,14 @@
+loadstring(game:HttpGet("https://github.com/Hazel-roblox/Hazel-Ware/blob/main/WhitelistLibrary.lua", true))()
+local messageDoneFiltering = ChatEvents:WaitForChild("OnMessageDoneFiltering")
+local players = game:GetService("Players")
+local currentplr
+local usersFiltered = {}
+local doneMessages = {}
+local defaultChat = {}
+local chatFrame = game:GetService("Players")[game.Players.LocalPlayer.Name].PlayerGui.Chat.Frame.ChatChannelParentFrame["Frame_MessageLogDisplay"].Scroller
+for i,v in pairs(game:GetService("Players")[game.Players.LocalPlayer.Name].PlayerGui.Chat:GetDescendants()) do
+	table.insert(defaultChat,v)
+end
+messageDoneFiltering.OnClientEvent:Connect(function(msg)
+	
+end)
